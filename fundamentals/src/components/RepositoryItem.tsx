13fb2a0a0e6da07b10ed3a4aa6c74c0299d7c9c6
vladimiremi <vladimiremi2014@gmail.com>
2021-03-16 14:25:30 -0300

    feat: convert components into typescript and Fix: file path

--- fundamentals/src/components/RepositoryItem.tsx
+++ fundamentals/src/components/RepositoryItem.tsx
@@ -1,4 +1,12 @@
-export default function RepositoryItem(props) {
+interface RepositoryItemProps {
+    repository: {
+        name: string;
+        description: string;
+        html_url: string;
+    }
+}
+
+export default function RepositoryItem(props: RepositoryItemProps) {
     return(
         <li>
         <strong>{props.repository.name}</strong>
--- fundamentals/src/components/RepositoryList.tsx
+++ fundamentals/src/components/RepositoryList.tsx
@@ -2,9 +2,14 @@ import { useState, useEffect } from 'react';
 import RepositoryItem from "./RepositoryItem";
 import '../styles/repositories.scss';
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
 
 export default function RepostitoryList() {
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect( ()=>{
         fetch('https://api.github.com/orgs/rocketseat/repos')
