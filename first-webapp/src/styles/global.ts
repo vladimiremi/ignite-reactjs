85aeb827ece7288d9ed0cd3f2ff43672545c0b0b
vladimiremi <vladimiremi2014@gmail.com>
2021-03-19 07:57:36 -0300

    chore: import fonts from Google Fonts

--- first-webapp/src/styles/global.ts
+++ first-webapp/src/styles/global.ts
@@ -38,6 +38,14 @@ export const GlobalStyle = createGlobalStyle`
         -webkit-font-smoothing: antialiased; //as fonts vão ficar mais nítidas
     }
 
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+
     button {
         cursor: pointer;
     }
