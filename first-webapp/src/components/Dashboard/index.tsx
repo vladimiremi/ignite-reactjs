63ec5fefa19f055d7ebf7d13469b8fb5535ab623
vladimiremi <vladimiremi2014@gmail.com>
2021-03-19 11:36:31 -0300

    feat: add summary

---
+++ first-webapp/src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Summary } from "../Summary";
+import { Container } from "../Dashboard/styles";
+
+export function Dashboard() {
+    return(
+        <Container>
+            <Summary />
+        </Container>
+    );
+}
