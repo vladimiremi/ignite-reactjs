71d23bc20936f101958bca35b6df43923fdeadc3
vladimiremi <vladimiremi2014@gmail.com>
2022-07-10 08:20:46 -0300

    feat: add NewTransactionModal component

--- first-webapp/src/App.tsx
+++ first-webapp/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement("#root");
@@ -21,14 +22,10 @@ export function App() {
     <>
       <Header openNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-        <text>Teste</text>
-      </Modal>
+      />
       <GlobalStyle />
     </>
   );
