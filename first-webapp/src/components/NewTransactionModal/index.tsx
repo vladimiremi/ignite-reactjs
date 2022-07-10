0ad08591f5e67cb48111ae5d51a1b44ed9735607
vladimiremi <vladimiremi2014@gmail.com>
2022-07-10 09:36:17 -0300

    feat: create type buttons

--- first-webapp/src/components/NewTransactionModal/index.tsx
+++ first-webapp/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
-import { Container } from "./styles";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import { Container, TransactionTypeContainer } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -25,6 +27,18 @@ export function NewTransactionModal({
         <h2>Cadastrar transação</h2>
         <input placeholder="Título" />
         <input placeholder="Valor" type="number" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Entrada</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
       </Container>
